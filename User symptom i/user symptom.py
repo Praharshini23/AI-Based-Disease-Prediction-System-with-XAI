3af551dc6eb3f13cda8b1b097f07aa194bc420f2
Praharshini Seth <s.praharshini05@gmail.com>
2026-06-16 21:58:56 +0530

    Create user symptom.py

---
+++ User symptom i/user symptom.py
@@ -0,0 +1,21 @@
+
+user_symptoms = pd.DataFrame(
+    [[0] * len(X_train.columns)],
+    columns=X_train.columns
+)
+
+user_symptoms["itching"] = 1
+user_symptoms["skin_rash"] = 1
+user_symptoms["nodal_skin_eruptions"] = 1
+
+prediction = model.predict(user_symptoms)
+
+print("\nPredicted Disease:", prediction[0])
+
+user_symptoms = pd.DataFrame(
+    [[0] * len(X_train.columns)],
+    columns=X_train.columns
+)
+
+for disease, prob in disease_probs[:3]:
+    print(f"{disease}: {prob*100:.2f}%")
